7e2b23662c513e55418f6c4a52eae5c8441a9d5f
itchyOwl <lauri.harkanen@gmail.com>
2018-09-23 09:01:51 +0300

    Add some string manipulation extension methods.

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,10 +1,23 @@
-﻿namespace ItchyOwl.Extensions
+﻿using System;
+using System.Linq;
+
+namespace ItchyOwl.Extensions
 {
     public static class StringExtensions
     {
         public static bool IsNullOrEmpty(this string s)
         {
             return string.IsNullOrEmpty(s);
+        }
+
+        public static string Remove(this string s, Func<char, bool> predicate)
+        {
+            return new string(s.ToCharArray().Where(c => !predicate(c)).ToArray());
+        }
+
+        public static string RemoveWhitespace(this string s)
+        {
+            return s.Remove(c => char.IsWhiteSpace(c));
         }
     }
 }
